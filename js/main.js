05910fcfa84e886112bc50add154d48c30c32d33
김예백 <yebaek02@naver.com>
2023-11-08 15:34:45 +0900

    11/8

--- js/main.js
+++ js/main.js
@@ -65,3 +65,33 @@ function hideSearch() {
   searchDelayEls.reverse()
   searchInputEl.value =''
 }
+
+// 요소의 가시성 관찰
+const io = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (!entry.isIntersecting) {
+      return
+    }
+    entry.target.classList.add('show')
+  })
+})
+const infoEls = document.querySelectorAll('.info')
+infoEls.forEach(function (el) {
+  io.observe(el)
+})
+
+// 비디오 재생!
+const video = document.querySelector('.stage video')
+const playBtn = document.querySelector('.stage .controller--play')
+const pauseBtn = document.querySelector('.stage .controller--pause')
+
+playBtn.addEventListener('click', function () {
+  video.play()
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+})
+pauseBtn.addEventListener('click', function () {
+  video.pause()
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+})
