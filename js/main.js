78e5cfc39dd2009f995615a06ed42ebaabf6e179
김예백 <yebaek02@naver.com>
2023-11-14 17:39:26 +0900

    11/14

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import ipads from '../data/ipads.js'
+import navigations from '../data/navigations.js'
 
 // 장바구니!
 const basketStarterEl = document.querySelector('header .basket-starter')
@@ -99,23 +100,56 @@ pauseBtn.addEventListener('click', function () {
 })
 
 // '당신에게 맞는 iPadsms?' 랜더링!
-const itemsEl = document.querySelector('section.copare .items')
+const itemsEl = document.querySelector('section.compare .items')
 ipads.forEach(function (ipad) {
   const itemEl = document.createElement('div')
   itemEl.classList.add('item')
+
+  let colorList = ''
+  ipad.colors.forEach(function (color) {
+    colorList += `<li style="background-color: ${color};"></li>`
+  })
+
   itemEl.innerHTML = /* html */ `
     <div class="thumbnail">
       <img src="${ipad.thumbnail}" alt="${ipad.name}" />
     </div>
     <ul class="colors">
-    
+      ${colorList}
     </ul>
     <h3 class="name">${ipad.name}</h3>
     <p class="tagline">${ipad.tagline}</p>
-    <p class="price">${ipad.price}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
     <button class="btn">구입하기</button>
     <a href="${ipad.url}" class="link">더 알아보기</a>
     `
 
   itemsEl.append(itemEl)
 })
+
+const navigationsEl = document.querySelector('footer .navigations')
+navigations.forEach(function (nav) {
+  const mapEl = document.createElement('div')
+  mapEl.classList.add('map')
+
+  let mapList = ''
+  nav.maps.forEach(function (map) {
+    mapList += /* html */ `<li>
+      <a href="${map.url}">${map.name}</a>
+    </li>`
+  })
+
+  mapEl.innerHTML = /* html */ `
+    <h3>
+      <span class="text">${nav.title}</span>
+    </h3>
+    <ul>
+      ${mapList}
+    </ul>
+  `
+
+  navigationsEl.append(mapEl)
+})
+
+const thisYearEl = document.querySelector('span.this-year')
+thisYearEl.textContent = new Date().getFullYear()
